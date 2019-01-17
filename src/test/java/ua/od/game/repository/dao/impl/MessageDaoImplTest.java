289bff68e747c66738ecbb370a74449cde896b0f
sekleta <sekleta.shylo@gmail.com>
2019-01-17 08:27:28 +0100

    Test MessegeImplTest

--- src/test/java/ua/od/game/repository/dao/impl/MessageDaoImplTest.java
+++ src/test/java/ua/od/game/repository/dao/impl/MessageDaoImplTest.java
@@ -2,6 +2,8 @@ package ua.od.game.repository.dao.impl;
 
 import org.junit.Before;
 import org.junit.Test;
+import ua.od.game.model.MessageEntity;
+import ua.od.game.repository.dao.DbTest;
 
 import static org.junit.Assert.*;
 
@@ -9,18 +11,22 @@ import static org.junit.Assert.*;
  * @author Andrey Kolodiy
  */
 
-public class MessageDaoImplTest {
+public class MessageDaoImplTest extends DbTest {
     MessageDaoImpl messageDao;
 
     @Before
-    public void init(){
-        messageDao = new MessageDaoImpl();}
+    public void init() {
+        messageDao = new MessageDaoImpl();
+    }
 
     @Test
     public void getMessageList() {
     }
 
     @Test
-    public void sendMessage() {
+    public void testSendMessage() {
+        Boolean message = messageDao.sendMessage(new MessageEntity());
+        System.out.println(message);
+        assertFalse(false);
     }
 }
