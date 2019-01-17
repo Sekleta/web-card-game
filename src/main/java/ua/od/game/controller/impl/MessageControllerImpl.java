1aca62146b5d3db49f98b00ce3b6e8c59b2dd2cd
sekleta <sekleta.shylo@gmail.com>
2019-01-17 21:24:00 +0100

    MesTest

--- src/main/java/ua/od/game/controller/impl/MessageControllerImpl.java
+++ src/main/java/ua/od/game/controller/impl/MessageControllerImpl.java
@@ -2,19 +2,31 @@ package ua.od.game.controller.impl;
 
 import ua.od.game.controller.MessageController;
 import ua.od.game.dto.MessageDto;
+import ua.od.game.service.MessageService;
 
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Date;
 import java.util.List;
 
+@Path("/message")
 public class MessageControllerImpl implements MessageController {
+
+    @Inject
+    public MessageService messageService;
 
     @Override
     public List<MessageDto> getMessageList(Integer fromAccountId, Integer toAccountId, Date fromTime) {
         return null;
     }
 
-    @Override
+    @POST
+    @Path("/account/message/send")
+    @Consumes({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public Response sendMessage(MessageDto message) {
         return null;
     }
--- src/test/java/ua/od/game/repository/dao/impl/MessageDaoImplTest.java
+++ src/test/java/ua/od/game/repository/dao/impl/MessageDaoImplTest.java
@@ -5,14 +5,18 @@ import org.junit.Test;
 import ua.od.game.model.MessageEntity;
 import ua.od.game.repository.dao.DbTest;
 
+import java.util.Date;
+
 import static org.junit.Assert.*;
 
 /**
- * @author Andrey Kolodiy
+ * @author Andrey Kolodiy, Sekleta
  */
 
 public class MessageDaoImplTest extends DbTest {
     MessageDaoImpl messageDao;
+
+    Date date = new Date(2019,11,12,00,53);
 
     @Before
     public void init() {
@@ -24,9 +28,16 @@ public class MessageDaoImplTest extends DbTest {
     }
 
     @Test
-    public void testSendMessage() {
-        Boolean message = messageDao.sendMessage(new MessageEntity());
-        System.out.println(message);
-        assertFalse(false);
+    public void sendMessage() {
+        MessageEntity message = new MessageEntity() {{
+            setText("testMessage");
+            setFromAccountId(111);
+            setToAccountId(222);
+            setTime(date);
+        }};
+
+        Boolean result = messageDao.sendMessage(message);
+        System.out.println(result);
+        assertTrue(result);
     }
 }
